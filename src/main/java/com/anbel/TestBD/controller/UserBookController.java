c644182dbf7d1693e95abe2719643c9738afbd83
anbel333 <anbel33333@mail.ru>
2024-02-16 11:27:39 +0400

    'addUserToBooks'

--- src/main/java/com/anbel/TestBD/controller/UserBookController.java
+++ src/main/java/com/anbel/TestBD/controller/UserBookController.java
@@ -2,11 +2,13 @@ package com.anbel.TestBD.controller;
 
 import com.anbel.TestBD.entity.Book;
 import com.anbel.TestBD.entity.Users;
+import com.anbel.TestBD.service.BookService;
 import com.anbel.TestBD.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -15,6 +17,8 @@ import java.util.List;
 public class UserBookController {
     @Autowired
     public UsersService usersService;
+    @Autowired
+    public BookService bookService;
     @GetMapping("/userBooks")
     public String getUserBooks(Long userId, Model model) {
         List<Book> userBooks = usersService.getUserBooks(userId);
@@ -24,5 +28,12 @@ public class UserBookController {
         String usersName = users.getFullName();
         model.addAttribute("userName", usersName);
         return "userBooks";
+    }
+    @PostMapping("/addBookToUsers")
+    public String addBookToUsers(Long userId, Book book) {
+        Users users = usersService.getUsersById(userId);
+        book.setUsers(users);
+        bookService.saveBook(book);
+        return "redirect:/userAccount?userId=" + userId;
     }
 }
