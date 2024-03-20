9ef7964ab5a7143fdfc3a489bf2a1d0dfb62a433
anbel333 <anbel33333@mail.ru>
2024-03-20 20:08:20 +0400

    JUnit

--- src/main/java/com/anbel/TestBD/service/BookService.java
+++ src/main/java/com/anbel/TestBD/service/BookService.java
@@ -6,7 +6,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 @RequiredArgsConstructor
 @Service
@@ -31,10 +30,10 @@ public class BookService {
     public List<Book> getBooksByUserId(Long userId) {
         return bookRepository.findByUsersId(userId);
     }
-    public void returnBook(Long userId, Long bookId) {
+    public Book returnBook(Long userId, Long bookId) {
         Book book = bookRepository.findById(bookId).orElseThrow();
         book.setUsers(null);
         book.setTakenByUser(false);
-        saveBook(book);
+        return bookRepository.save(book);
     }
 }
